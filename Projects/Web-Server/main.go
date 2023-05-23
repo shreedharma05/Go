c1e7a52c561e80e56982b427a72b738cf9658ccc
Shreedharma <skr524@outlook.com>
2023-05-23 16:17:06 +0530

    Web-Server project has been added

--- Projects/Web-Server/main.go
+++ Projects/Web-Server/main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		json.NewEncoder(w).Encode("Path not available")
+	}
+	json.NewEncoder(w).Encode("Um, Hello!")
+}
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("Name")
+	age := r.FormValue("Age")
+	address := r.FormValue("Address")
+
+	fmt.Printf("Name: %s\n", name)
+	fmt.Printf("Age: %s\n", age)
+	fmt.Printf("Address %s\n", address)
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("The server is starting at port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
